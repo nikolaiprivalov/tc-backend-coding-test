11cc2d44f26ac7f9e9f4e3706b703d4c025bb0c9
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2021-10-20 16:40:21 +0300

    edd error handling
    I'm using errors for flow control. It usually works nicely with the rails api.
    All the transaction errors or any other specific errors are rescued here.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveModel::ValidationError, with: :unprocessable_entity
+  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+
+  private
+
+  def record_not_found(error)
+    render json: { error: error.message }, status: :not_found
+  end
+
+  def unprocessable_entity(error)
+    render json: { error: error.message }, status: :unprocessable_entity
+  end
 end
